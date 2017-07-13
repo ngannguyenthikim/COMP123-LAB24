a03854ad4695d284141dd5b766b363b8e0c51cd6
300922400 <300922400@PR-A317-34>
2017-07-13 16:21:26 -0400

    Implemented the SuperVillain class

--- COMP123-S2017-Lesson9A/Program.cs
+++ COMP123-S2017-Lesson9A/Program.cs
@@ -5,10 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 /*
- * Name: Tom Tsiliopoulos
- * Date: July 11, 2017
+ * Name:Nguyen Thi Kim Ngan
+ * Date: July 13, 2017
  * Description: This is the driver class for my project
- * Version: 0.4 - Implemented the SuperHero class
+ * Version: 0.5 - Implemented the SuperVillain class
  */
 
 namespace COMP123_S2017_Lesson9A
@@ -17,13 +17,21 @@ namespace COMP123_S2017_Lesson9A
     {
         static void Main(string[] args)
         {
-            SuperHero superHero = new SuperHero("SuperHero Dude", 20);
-            superHero.Powers.Add(new Power("Teleportation",50));
-            superHero.Powers.Add(new Power("Phasing", 40));
-            superHero.Powers.Add(new Power("Invisibility", 30));
-            Console.WriteLine(superHero.ToString());
-            Console.WriteLine("Karma: " + superHero.Karma);
-
+            //SuperHero superHero = new SuperHero("SuperHero Dude", 20);
+            //superHero.Powers.Add(new Power("Teleportation", 50));
+            //superHero.Powers.Add(new Power("Phasing", 40));
+            //superHero.Powers.Add(new Power("Invisibility", 30));
+            //Console.WriteLine(superHero.ToString());
+            //Console.WriteLine("Karma: " + superHero.Karma);
+            SuperVillain superVillain1 = new SuperVillain("SupperVillain Alan", 10);
+            superVillain1.Powers.Add(new Power("War", 20));
+            superVillain1.Powers.Add(new Power("Disease", 40));
+            Console.WriteLine(superVillain1.ToString());
+            Console.WriteLine("Malice: " + superVillain1.Malice);
+            SuperVillain superVillain2 = new SuperVillain("SupperVillain Angela", 15);
+            superVillain2.Powers.Add(new Power("Fire", 35));
+            Console.WriteLine(superVillain2.ToString());
+            Console.WriteLine("Malice: " + superVillain2.Malice);
         }
     }
 }
